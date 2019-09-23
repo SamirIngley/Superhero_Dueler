da122944fcc5dda742ea12bd15d0bd893be5410d
SamirIngley <samir.ingle7@gmail.com>
2019-09-23 15:29:17 -0700

    working on fight

--- superhero.py
+++ superhero.py
@@ -18,10 +18,10 @@ class Armor:
         self.max_block = max_block
 
     def block(self):
-        return random.randint(0, max_block)
+        return random.randint(0, self.max_block)
 
 class Hero:
-    def __init__(self, name, starting_health=100):
+    def __init__(self, name, starting_health):
         '''Instance properties:
           abilities: List
           armors: List
@@ -38,25 +38,69 @@ class Hero:
     def add_ability(self, ability):
         self.abilities.append(ability)
 
+    def attack(self):
+        for ability in self.abilities:
+            print(f' Ability: {ability.name}')
+            return ability.attack()
 
-    def attack():
-        pass
+    def add_armor(self, armor):
+        self.armors.append(armor)
 
-    def defend(incoming_damage):
-        pass
+    def defend(self):
+        for armor in self.armors:
+            print(f' Defense: {armor.name}')
+            return armor.block()
 
-    def take_damage(damage):
-        pass
+    def take_damage(self, incoming_damage):
+        self.current_health -= incoming_damage
 
-    def is_alive():
-        pass
 
-    def fight(HeroClass):
-        pass
+    def is_alive(self):
+        if self.current_health > 0:
+            return True
+        else:
+            return False
+
+    def fight(self, opponent):
+        while self.is_alive() and opponent.is_alive():
+
+            first_attack = self.attack()
+            opponent_attack = opponent.attack()
+
+            opponent.take_damage(first_attack)
+            self.take_damage(opponent_attack)
+
+        if self.is_alive() == False and opponent.is_alive() == False:
+            print("Stalemate!")
+        elif self.is_alive() == False:
+            print(f"{opponent.name} won!")
+        else:
+            print(f"{self.name} won!")
+
+
 
 
 if __name__ == "__main__":
-    ability = Ability("Great Debugging", 50)
-    hero = Hero("Grace Hopper", 200)
-    hero.add_ability(ability)
-    print(hero.abilities)
+
+    SuperMan = Hero("SuperMan", 200)
+    print(f" Name: {SuperMan.name}")
+    print(f" Health: {SuperMan.current_health}")
+
+    lazereyes = Ability("lazer 👁", 100)
+    SuperMan.add_ability(lazereyes)
+
+    steelbody = Armor("steelbody  🦹🏾‍♂️", 100)
+    SuperMan.add_armor(steelbody)
+
+    print(f' Defense Power: {SuperMan.defend()}')
+    print(f' Ability power : {SuperMan.attack()}')
+
+    SuperMan.take_damage(59)
+    print(f' current health: {SuperMan.current_health}')
+
+    SuperMan.take_damage(120)
+    print(SuperMan.is_alive())
+    SuperMan.take_damage(300)
+    print(SuperMan.is_alive())
+
+    SuperMan.fight(yourmom)
