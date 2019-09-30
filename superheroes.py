fefa2f308d8ab413541e31446192393e5add889b
SamirIngley <samir.ingle7@gmail.com>
2019-09-29 18:22:58 -0700

    working on arena still 1 error in test

--- superheroes.py
+++ superheroes.py
@@ -41,19 +41,22 @@ class Hero:
         self.armors = list()
         self.current_health = starting_health
         self.starting_health = starting_health
+        self.deaths = 0
+        self.kills = 0
+
 
     def add_ability(self, ability):
         self.abilities.append(ability)
 
     def add_weapon(self, weapon):
-        self.weapons.append(weapon)
+        self.abilities.append(weapon)
 
     def attack(self):
-        total_attack = 0
+
         for ability in self.abilities:
             #print(f' Ability: {ability.name}')
-            total_attack += ability.attack()
-        return total_attack
+            return ability.attack()
+
 
     def add_armor(self, armor):
         self.armors.append(armor)
@@ -73,6 +76,14 @@ class Hero:
         else:
             return False
 
+    #Tracking statistics
+    def add_kill(self, num_kills):
+        self.kills += num_kills
+
+    def add_deaths(self, num_deaths):
+        self.deaths += num_deaths
+
+
     def fight(self, opponent):
         while self.is_alive() and opponent.is_alive():
 
@@ -86,8 +97,10 @@ class Hero:
             print("Stalemate!")
         elif self.is_alive() == False:
             print(f"{opponent.name} won!")
+            add_deaths(1)
         else:
             print(f"{self.name} won!")
+            add_kill(1)
 
 class Team:
     def __init__(self, name):
@@ -107,6 +120,72 @@ class Team:
     def view_all_heroes(self):
         for hero in self.heroes:
             print(hero.name)
+
+    def attack(self, other_team):
+        your_hero = random.choice(self.heroes)
+        opponent_hero = random.choice(other_team)
+        your_hero.fight(opponent)
+
+    def revive_heroes(self, health=100):
+        for hero in self.heroes:
+            hero.current_health = health
+
+    def stats(self):
+        # total_kills = 0
+        # total_deaths = 0
+        # kdr = 0
+        # for hero in self.heroes:
+        #     hero.add_kills(hero.kills)
+        #     total_deaths += hero.num_deaths
+        total_kills = 0
+        total_deaths = 0
+        kdr = 0
+        for hero in self.heroes:
+            total_kills += hero.kills
+            total_deaths += hero.deaths
+        if total_deaths == 0:
+            kdr = total_deaths
+        else:
+            kdr = total_kills/total_deaths
+        return kdr
+
+
+
+
+
+
+class Arena:
+    def __init__(self):
+        team_one: None
+        team_two: None
+
+    def create_ability(self):
+        name = input("What's the ability's name?")
+        max_damage = int(input("Max Damage?"))
+        return Ability(name, max_damage)
+
+    def create_weapon(self):
+        name = ("What's the weapon's name?")
+        max_damage = int(input("Max Damage"))
+        return Weapon(name, max_damage)
+
+    def create_armor(self):
+        name = input("What's the armor name")
+        max_block = input("Max Defense?")
+        return Armor(name, max_block)
+
+    def create_hero(self):
+        name = input("Hero name?")
+        new_Hero = Hero(name, starting_health = 200)
+        while (input("Add abilities? Y/N")) == ("Y" or "y"):
+            new_Hero.add_ability(self.create_ability())
+            print(f"Added {ability_name}")
+        while (input("Add weapons? Y/N")) == ("Y" or "y"):
+            new_Hero.add_weapon(self.create_weapon())
+            print(f"Added {weapon_name}")
+        while (input("Add armor? Y/N")) == ("Y" or "y"):
+            new_Hero.add_armor(self.create_armor())
+            print(f"Added {armor_name}")
 
 
 
